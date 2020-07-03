675b29231e6d6d867b18e1656c119142531600e1
duydoan <duydoan1411@gmail.com>
2020-07-03 14:02:34 +0700

    Chapter 7

--- app/code/Magenest/Movie/Plugin/Cart/CartImage.php
+++ app/code/Magenest/Movie/Plugin/Cart/CartImage.php
@@ -7,13 +7,33 @@ namespace Magenest\Movie\Plugin\Cart;
 class CartImage
 {
     private $logger;
+    public $productCollectionFactory;
 
-    public function __construct(\Psr\Log\LoggerInterface $logger)
+    public function __construct(\Psr\Log\LoggerInterface $logger,
+                                \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory
+                                $productCollectionFactory)
     {
         $this->logger = $logger;
+        $this->productCollectionFactory = $productCollectionFactory;
     }
-//    public function afterGetImage($product, $imageId, $attributes = []){
-////        $this->logger->debug('done!!!!!!!!!!!!!1');
-////        return $this->imageBuilder->create($product, $imageId, $attributes);
+
+//    public function beforeAddProduct($subject, $productInfo, $requestInfo){
+//        if ($requestInfo['selected_configurable_option']){
+//            $collection = $this->productCollectionFactory->create();
+//            $product = $collection->getItemById($requestInfo['selected_configurable_option']);
+//            $productInfo->setData($product->getData());
+//            $requestInfo['product'] = $requestInfo['selected_configurable_option'];
+//            $requestInfo['item'] = $requestInfo['selected_configurable_option'];
+//            $requestInfo['selected_configurable_option'] = '';
+//        }
+//        return [$productInfo, $requestInfo];
 //    }
+
+    public function beforeExecute(\Magento\Checkout\Controller\Cart\Add $subject){
+        if ($_REQUEST['selected_configurable_option']) {
+            $producId = $_REQUEST['selected_configurable_option'];
+            $subject->getRequest()->setParams(['product' => $producId]);
+        }
+    }
+
 }
